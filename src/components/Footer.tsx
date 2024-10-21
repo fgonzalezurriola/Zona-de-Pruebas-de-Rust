2d91695068486727677b219ea364b1ce59390351
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-10-21 14:30:53 -0300

    Refactor: Added links for portfolio and source code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
-      <p className="text-sm">Powered by Rust Playground API</p>
+    <footer className="bg-gray-800 text-white p-4 text-center flex items-center justify-center">
+      <p className="text-sm">Impulsado por la API de Rust Playground</p> 
+      <p className="text-sm ml-12 text-orange-500">Hecho por fgonzalezurriola</p> 
     </footer>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Code2 } from 'lucide-react';
+import { FaGithub, FaBriefcase } from 'react-icons/fa';
+
 
 const Header: React.FC = () => {
   return (
     <header className="bg-orange-600 text-white p-4 shadow-md">
-      <div className="container mx-auto flex items-center justify-center">
-        <Code2 className="mr-2" size={24} />
-        <h1 className="text-2xl font-bold">Zona-de-Pruebas-de-Rust</h1>
+      <div className="container mx-auto flex items-center justify-between">
+        <a href="https://users.dcc.uchile.cl/~fragonza/" className="flex items-center justify-start">
+            <FaBriefcase   className="text-5xl text-gray-800 hover:text-white" />
+        </a>
+        <a href="https://github.com/fgonzalezurriola/Zona-de-Pruebas-de-Rust" className="flex items-center justify-start">
+          <FaGithub className="text-5xl text-gray-800 hover:text-white" />
+        </a>
+
+        
+        <div className="absolute left-1/2 transform -translate-x-1/2 flex items-center">
+          <Code2 className="mr-2" size={32} /> 
+          <h1 className="text-4xl font-bold">Zona-de-Pruebas-de-Rust</h1>
+        </div>
       </div>
     </header>
+
   );
 };
 
