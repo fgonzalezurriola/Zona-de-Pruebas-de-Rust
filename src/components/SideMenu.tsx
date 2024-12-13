7e965f7bb4249b289b2476c2d41fd295c3b41f55
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-12-13 19:19:37 -0300

    Styles: updated styles for the SideMenu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -11,14 +11,14 @@ const problems = [
   { id: 7, title: "Es Número Primo", code: 'fn es_primo(n: u32) -> bool {\n    if n <= 1 {\n        return false;\n    }\n    for i in 2..n {\n        if n % i == 0 {\n            return false;\n        }\n    }\n    true\n}\n\nfn main() {\n    let numero = 29;\n    println!("¿Es {} primo? {}", numero, es_primo(numero));\n}' },
   { id: 8, title: "Two Sum", code: 'use std::collections::HashMap;\n\nfn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {\n    let mut mapa: HashMap<i32, usize> = HashMap::new();\n\n    for (i, &num) in nums.iter().enumerate() {\n        let complemento = target - num;\n        if let Some(&indice) = mapa.get(&complemento) {\n            return vec![indice as i32, i as i32];\n        }\n        mapa.insert(num, i);\n    }\n    \n    vec![] \n}\n\nfn main() {\n    let nums = vec![2, 7, 11, 15];\n    let target = 9;\n    let resultado = two_sum(nums, target);\n    \n    println!("Los índices que suman {} son: {:?}", target, resultado);\n}' },
   { id: 9, title: "Propiedad y Préstamos", code: 'fn main() {\n    let s1 = String::from("Hello, Rust!");\n    let len = calcular_longitud(&s1); \n    \n    println!("La longitud de \'{}\' es {}.", s1, len);\n}\n\nfn calcular_longitud(s: &String) -> usize {\n    s.len() \n}' },
-  { id: 10, title: "Manejo de Option y Result", code: 'fn dividir(dividendo: f64, divisor: f64) -> Result<f64, String> {\n    if divisor == 0.0 {\n        Err(String::from("Error: división entre cero"))\n    } else {\n        Ok(dividendo / divisor)\n    }\n}\n\nfn main() {\n    let resultado = dividir(10.0, 2.0);\n    match resultado {\n        Ok(cociente) => println!("El resultado es {}", cociente),\n        Err(error) => println!("{}", error),\n    }\n\n    let error_resultado = dividir(10.0, 0.0);\n    match error_resultado {\n        Ok(cociente) => println!("El resultado es {}", cociente),\n        Err(error) => println!("{}", error),\n    }\n}' },
+  { id: 10, title: "Option y Result", code: 'fn dividir(dividendo: f64, divisor: f64) -> Result<f64, String> {\n    if divisor == 0.0 {\n        Err(String::from("Error: división entre cero"))\n    } else {\n        Ok(dividendo / divisor)\n    }\n}\n\nfn main() {\n    let resultado = dividir(10.0, 2.0);\n    match resultado {\n        Ok(cociente) => println!("El resultado es {}", cociente),\n        Err(error) => println!("{}", error),\n    }\n\n    let error_resultado = dividir(10.0, 0.0);\n    match error_resultado {\n        Ok(cociente) => println!("El resultado es {}", cociente),\n        Err(error) => println!("{}", error),\n    }\n}' },
 ];
 
 const SideMenu: React.FC<{ onSelectProblem: (problem: any) => void }> = ({ onSelectProblem }) => {
   const [isExpanded, setIsExpanded] = useState(true);
 
   return (
-    <div className={`bg-gray-800 text-white transition-all duration-300 ${isExpanded ? 'w-64' : 'w-16'} overflow-y-auto`}>
+    <div className={`bg-gray-800 text-white transition-all duration-300 ${isExpanded ? 'sm:w-12 lg:w-72' : 'w-12'} overflow-y-auto`}>
       <button
         className="w-full p-4 flex justify-end items-center"
         onClick={() => setIsExpanded(!isExpanded)}
@@ -29,10 +29,10 @@ const SideMenu: React.FC<{ onSelectProblem: (problem: any) => void }> = ({ onSel
         {problems.map((problem) => (
           <button
             key={problem.id}
-            className="w-full text-left p-4 hover:bg-gray-700 transition-colors duration-200"
+            className="w-full text-left p-4 hover:bg-gray-700 transition-colors duration-100 whitespace-nowrap"
             onClick={() => onSelectProblem(problem)}
           >
-            {isExpanded ? `Problem ${problem.id}: ${problem.title}` : problem.id}
+            {isExpanded ? `P${problem.id}: ${problem.title}` : problem.id}
           </button>
         ))}
       </nav>
